4f9a24db0463fe42b7f6b8219c187af57c4fe72b
alennas1 <Alounnas2002@gmail.com>
2025-09-13 22:14:18 +0100

    yaaay

--- src/main/java/com/schoolapp/teacher_manager/repository/TeacherRepository.java
+++ src/main/java/com/schoolapp/teacher_manager/repository/TeacherRepository.java
@@ -1,9 +1,9 @@
 package com.schoolapp.teacher_manager.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.schoolapp.teacher_manager.model.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
-    
 }
